97a28f33839878bc4e98a0d05270e7f4e907dd13
Frei Prisco <prisco.frei@alpiq.com>
2023-03-21 22:17:41 +0100

    blub

--- tier_2/data_handling/check_issue_load_dataset.py
+++ tier_2/data_handling/check_issue_load_dataset.py
@@ -3,3 +3,5 @@ import climetlab as cml
 URL = "https://www.ncei.noaa.gov/data/international-best-track-archive-for-climate-stewardship-ibtracs/v04r00/access/csv/ibtracs.SP.list.v04r00.csv"
 data = cml.load_source("url", URL)
 pd = data.to_pandas()
+
+forecast_error = cml.load_dataset("mltc-surface-observation-postprocessing", field="forecast_error").to_numpy()
